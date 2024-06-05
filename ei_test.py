93aeb04cac51e691c3695a16b8239523291f9a4d
eithetpainghmuu <ei.thet.paing.hmuu@mic-p.com>
2024-06-05 17:40:58 +0900

    test_file_update

---
+++ ei_test.py
@@ -0,0 +1,63 @@
+#bubble sort
+
+# list = [6,4,2,1,3,5]
+# print(list)
+
+# for i in range(len(list)):
+    
+#     for j in range(len(list)-1):
+        
+#         if list[j] > list[j+1]: 
+#            list[j], list[j+1] = list[j+1], list[j]
+
+# print(list)
+ 
+
+# list1 = [6,4,2,1,3,5]
+# y = 4
+
+# for x in list1:
+#     if x == y:
+#      print(x)
+#     else:
+#      print("Not Found")
+
+# search_index = 6
+# if (search_index == None):
+#     print("not found")
+# else :
+#    print(search_index)
+
+
+# try:
+#     # dd
+#     print("dd")
+# except:
+#     print("log")
+# else:
+#     print("no error")
+# finally:
+#     print("printtttt")
+# "名前{0}です。{1}歳。住所は{2}です。".format("Yamada", 18, "Tokyo")
+
+# line = "名前{name}です。{age}歳。住所は{address}です。".format(name= "イテッパインムー",age= 27,address= "三鷹")
+
+# print(line)
+
+
+# data = "123-34555"
+# data = data.replace("-","")
+# print(data)
+
+
+# data1 = "ppgg--__!!!"
+
+# data1 = data1.replace("--__!!!","")
+# print(data1)
+
+
+
+
+
+
+
