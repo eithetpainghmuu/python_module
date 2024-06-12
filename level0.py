1f087cdf8eb7934255e25cb0782eb2fee8ce29dd
eithetpainghmuu <ei.thet.paing.hmuu@mic-p.com>
2024-06-12 17:38:46 +0900

    level0.1 update

--- level0.py
+++ level0.py
@@ -1,11 +1,11 @@
 numbers = [1,2,3,4,6,7,8,0]
 
 def solution(numbers):
-    list1 = []
+    list = []
     for total in range(0,10):
         if total not in numbers:
-            list1.append(total)
-            result = sum(list1)
+            list.append(total)
+            result = sum(list)
             
     return result
 print(solution(numbers))
