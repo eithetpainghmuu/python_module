43307e2683a56057f8a0d008f97374bdd1568e70
eithetpainghmuu <ei.thet.paing.hmuu@mic-p.com>
2024-06-12 17:32:54 +0900

    update

--- level0.py
+++ level0.py
@@ -1,16 +1,6 @@
 numbers = [1,2,3,4,6,7,8,0]
 
 def solution(numbers):
-    num1 = numbers[0]
-    for i in numbers:
-        if i > num1:
-            num1 = i
-
-    num2 = numbers[0]
-    for i in numbers:
-        if i < num2:
-            num2 = i
-
     list1 = []
     for total in range(0,10):
         if total not in numbers:
