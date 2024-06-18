d4a3a13e9a9838267daf00c792aafd5aa8ca9608
eithetpainghmuu <ei.thet.paing.hmuu@mic-p.com>
2024-06-18 09:13:36 +0900

    update

--- level0.py
+++ level0.py
@@ -10,3 +10,29 @@ def solution(numbers):
     return result
 print(solution(numbers))
 
+#####################################################
+
+n = 10
+
+def solution(n):
+    for x in range(0,11):
+        x += 1
+        if (n % x) == 1:         
+         return x
+
+
+print(solution(n))
+
+#####################################################
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8]
+n = 2
+
+###### 変更箇所 ##########
+def solution(num_list, n):
+    result = [num_list[i:i+n] for i in range(0, len(num_list), n)]
+    return result
+
+###### 変更箇所 ##########
+
+print(solution(num_list, n))
